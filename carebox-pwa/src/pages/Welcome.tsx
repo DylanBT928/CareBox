df7e18d362cd7d60d1a31d77d0ab442f39c6062d
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2025-05-24 02:36:13 -0700

    Added Welcome page

---
+++ carebox-pwa/src/pages/Welcome.tsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+export default function Welcome() {
+  return (
+  <div>
+    <div>CareBox</div>
+    <div>
+      <button>Log in</button>
+      <button>Sign Up</button>
+    </div>
+  </div>
+    );
+}
