a3d8a02bd24a1cb135f1a8bcd7a72217e2dea960
Dylan Tran <dylanbt928@gmail.com>
2025-05-23 23:49:31 -0700

    setup navbar

---
+++ carebox-pwa/src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className={pathname === "/" ? "active" : ""}>
+        Home
+      </Link>
+      <Link to="/add" className={pathname === "/add" ? "active" : ""}>
+        Add
+      </Link>
+      <Link to="/history" className={pathname === "/history" ? "active" : ""}>
+        History
+      </Link>
+      <Link to="/settings" className={pathname === "/settings" ? "active" : ""}>
+        Settings
+      </Link>
+    </nav>
+  );
+}
