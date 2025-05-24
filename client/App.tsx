dbc195effd4381f30e890f86318af88233d9a3b8
Dylan Tran <dylanbt928@gmail.com>
2025-05-23 22:12:02 -0700

    setup tabbar

--- client/App.tsx
+++ client/App.tsx
@@ -1,14 +1,25 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./src/screens/Home";
+import Add from "./src/screens/Add";
+import History from "./src/screens/History";
+import Settings from "./src/screens/Settings";
+import BlurTabBar from "./src/components/TabBar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-purple-700">
-        Welcome to CareBox
-      </Text>
-      <Text className="mt-2 text-base text-gray-600">
-        Track your health supplies with ease
-      </Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tab.Navigator tabBar={(props) => <BlurTabBar {...props} />}>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Add" component={Add} />
+          <Tab.Screen name="History" component={History} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
