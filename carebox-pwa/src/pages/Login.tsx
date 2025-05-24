8980a019abea150e5b8168482f9a5069e7674f3d
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2025-05-24 09:38:00 -0700

    Added Login Page

---
+++ carebox-pwa/src/pages/Login.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import "./SignUp.css";
+
+type Credentials = {
+  email: string;
+  password: string;
+};
+
+export default function SignUp() {
+  const [credentialsValue, setcredentialsValue] = useState<Credentials>({
+    email: '',
+    password: '',
+    });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setcredentialsValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Form submitted:', credentialsValue);
+    alert(`Name: ${credentialsValue.email}, Email: ${credentialsValue.email}`);
+    const auth = getAuth();
+signInWithEmailAndPassword(auth, credentialsValue.email, credentialsValue.password)
+  .then((_userCredential) => {
+        alert("Logged in successfully!");
+        navigate("/home");
+  })
+  .catch((error) => {
+        alert("Error signing up: " + error.message);    
+  });
+};
+
+  return (
+  <div>
+    <div>CareBox</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Enter email:</label>
+      <input
+        type="email"
+        id="email"
+        value={credentialsValue.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="password">Enter password:</label>
+      <input
+        type="password"
+        id="password"
+        value={credentialsValue.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  </div>
+    );
+}
