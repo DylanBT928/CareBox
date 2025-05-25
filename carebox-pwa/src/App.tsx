f1edb286b013891f02ef88c7fd341ee464a54a32
derekxu2023k <dkxu@uci.edu>
2025-05-24 17:38:59 -0700

    Make dark mode work when landing on any page

--- carebox-pwa/src/App.tsx
+++ carebox-pwa/src/App.tsx
@@ -7,10 +7,27 @@ import Home from "./pages/Home";
 import AddItem from "./pages/AddItem";
 import Map from "./pages/Map";
 import Settings from "./pages/Settings";
+import { useEffect, useState } from "react";
 
 function AppRoutes() {
   const { pathname } = useLocation();
   const showNavbar = !["/", "/signup", "/login"].includes(pathname);
+  
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      return savedTheme === "dark";
+    }
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      darkMode ? "dark" : "light"
+    );
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+  }, [darkMode]);
 
   return (
     <>
@@ -22,7 +39,7 @@ function AppRoutes() {
           <Route path="/home" element={<Home />} />
           <Route path="/add" element={<AddItem />} />
           <Route path="/map" element={<Map />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings darkMode={darkMode} setDarkMode={setDarkMode}/>} />
         </Routes>
       </div>
       {showNavbar && <Navbar />}
