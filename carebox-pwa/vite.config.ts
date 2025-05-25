085b28035f064ae9bb20b93a6f301a88ee10ef6d
Dylan Tran <dylanbt928@gmail.com>
2025-05-24 17:29:11 -0700

    add vite icon

--- carebox-pwa/vite.config.ts
+++ carebox-pwa/vite.config.ts
@@ -24,19 +24,21 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icon-192x192.png",
+            src: "/src/assets/logo.svg",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/svg+xml",
+            purpose: "any",
           },
           {
-            src: "/icon-512x512.png",
+            src: "/src/assets/logo.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
+            purpose: "any",
           },
           {
-            src: "/icon-512x512.png",
+            src: "/src/assets/logo.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
             purpose: "any maskable",
           },
         ],
